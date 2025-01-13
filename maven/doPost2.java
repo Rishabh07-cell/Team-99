9257753d39ef9964cb9570fa30d088f7b360b2c1
Rishabh07-cell <rajsinghsingh8485@gmail.com>
2025-01-13 17:05:43 +0530

    Delete maven directory

--- maven/doPost2.java
+++
@@ -1,6 +0,0 @@
-@Override
-protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    String studentID = request.getParameter("studentID");
-    String name = request.getParameter("name");
-    // Save the data
-}
--- maven/doget.java
+++
@@ -1,6 +0,0 @@
-@Override
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    // Example: Forwarding request to a JSP to display grades
-    RequestDispatcher dispatcher = request.getRequestDispatcher("/grades.jsp");
-    dispatcher.forward(request, response);
-}
